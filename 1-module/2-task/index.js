cb35c8b74c3b12157df8027eb07225b28323ff3e
Fedya Mercuriev <gleb.gorkoltsev@yandex.ru>
2018-10-15 00:00:01 +0700

    Решил задачу 1-2

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -12,6 +12,12 @@ function print (text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid (name) {
+  if (name !== "" && name.length >= 4 && name.indexOf(" ") === -1) {
+    return true;
+  } else {
+    return false;
+  }
+
 }
 
 function sayHello () {
@@ -25,4 +31,3 @@ function sayHello () {
 }
 
 sayHello();
-
