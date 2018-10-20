a7a464dbdc8114a2a00fa3ec6fc6934038dd5c37
Fedya Mercuriev <gleb.gorkoltsev@yandex.ru>
2018-10-21 01:11:43 +0700

    Решил 1 задачу 3 модуля

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -5,6 +5,15 @@
  * @returns {string}
  */
 function showSalary(data, age) {
+  let outputResult = "";
+  for (let i = 0; i < data.length; i++) {
+    if (data[i]['age'] <= age) {
+      if (outputResult !== "") {
+        outputResult += "\n";
+      }
+      outputResult += `${data[i].name}, ${data[i].balance}`;
+    }
+  }
+  outputResult.slice(outputResult.lastIndexOf('\n'), outputResult.lastIndexOf('\n') + 2);
+  return outputResult;
 }
-
-
