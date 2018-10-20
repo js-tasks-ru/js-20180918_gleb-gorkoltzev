0788869246931480ef8b7f5f0d7c2b365ad2fd70
Fedya Mercuriev <gleb.gorkoltsev@yandex.ru>
2018-10-21 01:05:05 +0700

    Оптимизировал задачу

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -7,28 +7,31 @@
  */
 function getMinMax(str) {
   let temp = "",
-			min = 0,
-			max = 0,
+      result = [],
+			// min = 0,
+			// max = 0,
 			index = 0;
 	while (index < str.length) {
 		if (str[index].match(/[-\d.]/)) {
 			temp += str[index];
 		};
 		if (str[index].indexOf(",") !== -1 || str[index].indexOf(" ") !== -1 || index === str.length - 1) {
-			if (temp && +temp <= min) {
-				min = +temp;
-				temp = "";
-			}
-			if (temp && +temp >= max) {
-				max = +temp;
-				temp = "";
-			}
+      result.push(+temp);
+			// if (temp && +temp <= min) {
+			// 	min = +temp;
+			// 	temp = "";
+			// }
+			// if (temp && +temp >= max) {
+			// 	max = +temp;
+			// 	temp = "";
+			// }
 			temp = "";
 		}
 		index++;
 	}
+  alert(`min: ${Math.min(...result)}; max: ${Math.max(...result)}`);
 	return {
-		'min': min,
-		'max': max
+		'min': Math.min(...result),
+		'max': Math.max(...result)
 	}
 }
