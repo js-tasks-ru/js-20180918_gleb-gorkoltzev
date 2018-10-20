ba5c8309fff3eefc5c916e9d06b3255376c2b33d
Fedya Mercuriev <gleb.gorkoltsev@yandex.ru>
2018-10-21 00:55:33 +0700

    Решил 3 задачу 1 модуля

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -2,10 +2,33 @@
 
 /**
  * Найти min/max в произвольной строке
- * @param   {string} str -  входные данные
+ * @param   {str} str -  входные данные
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
-
+  let temp = "",
+			min = 0,
+			max = 0,
+			index = 0;
+	while (index < str.length) {
+		if (str[index].match(/[-\d.]/)) {
+			temp += str[index];
+		};
+		if (str[index].indexOf(",") !== -1 || str[index].indexOf(" ") !== -1 || index === str.length - 1) {
+			if (temp && +temp <= min) {
+				min = +temp;
+				temp = "";
+			}
+			if (temp && +temp >= max) {
+				max = +temp;
+				temp = "";
+			}
+			temp = "";
+		}
+		index++;
+	}
+	return {
+		'min': min,
+		'max': max
+	}
 }
-
